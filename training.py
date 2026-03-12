7afd778ee5f0c04650a6b0e633d99dc6eee698ac
Thorfinn-19 <ukognos19@gmail.com>
2026-03-12 23:45:39 +0100

    Update training.py

--- training.py
+++ training.py
@@ -3,11 +3,10 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 model.train(
     data="cards.yaml", 
-    imgsz=512,
+    imgsz=640,
     epochs=50,
     batch=-1,
     device=0,
-    workers=2,
     project="runs",
     name="card_detector"
 )
